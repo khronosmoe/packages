533dfb89bd5e495742a905790a3c8fcc6ab70750
Khronos <khronos@Khronoss-Mac-mini.local>
2022-07-17 23:54:48 +0800

    add tls support

--- packages/gateway/src/main.ts
+++ packages/gateway/src/main.ts
@@ -1,3 +1,36 @@
-import { startServer } from './index';
+import express from 'express';
+import * as fs from 'fs';
+import * as http from 'http';
+import * as https from 'https';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import { AppExceptionsFilter } from './filter/app-exception.filter';
 
-startServer();
+export async function bootstrap(){
+  const server = express();
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+  app.useGlobalFilters(new AppExceptionsFilter());
+  app.use(express.json());  
+  await app.init();
+
+  if (fs.existsSync('./tls.yaml')) {
+    console.log("tls configuration exists");
+    const yenv = require('yenv');
+    const env = yenv('tls.yaml', { env: 'tls_server_config' });
+    const key_file = env.key_file
+    const cert_file = env.cert_file
+    const httpsOptions = {  
+      key: fs.readFileSync(key_file),
+      cert: fs.readFileSync(cert_file),
+    };
+    https.createServer(httpsOptions, server).listen(3001);
+  }else {
+    http.createServer(server).listen(3000);
+  }
+  
+}
